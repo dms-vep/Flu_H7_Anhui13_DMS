c789919110b033124e9ec40329df102881b816fc
jennyahn0 <jenny.ahn0@gmail.com>
2025-08-12 11:20:25 -0700

    updated ph selections from spinfection v2

--- scratch_notebooks/dmsviz/configure_dms_viz.sh
+++ scratch_notebooks/dmsviz/configure_dms_viz.sh
@@ -1,12 +1,25 @@
 #!/bin/bash
 
 configure-dms-viz format \
-   --name "H7 Functional Scores" \
-   --input "293T_entry_func_effects_sequential.csv" \
+   --name "H7 stability Scores 2_6" \
+   --input "2-6_entry_func_effects_sequential.csv" \
    --metric "effect" \
-   --metric-name "Effect" \
-   --structure "4r8w_timer.pdb" \
+   --metric-name "effect" \
+   --structure "4r8w_trimer.pdb" \
    --heatmap-limits '-4, 0 , 1' \
    --included-chains 'A B' \
    --excluded-chains 'L H' \
-   --output h7_functional_effects.json
+   --output H7_2-6_functional.json
+
+
+
+   # configure-dms-viz format \
+   # --name "H7 stability Scores" \
+   # --input "293T_entry_func_effects_sequential.csv" \
+   # --metric "effect" \
+   # --metric-name "Effect" \
+   # --structure "4r8w_timer.pdb" \
+   # --heatmap-limits '-4, 0 , 1' \
+   # --included-chains 'A B' \
+   # --excluded-chains 'L H' \
+   # --output h7_stability_effects.json
